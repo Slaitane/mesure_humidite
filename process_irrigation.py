aa24146850d481db5b1aeb3b7c342919083b2f7a
Slaitane <75116814+Slaitane@users.noreply.github.com>
2020-12-18 11:14:38 +0100

    Create process_irrigation.py

---
+++ process_irrigation.py
@@ -0,0 +1,42 @@
+"""
+Documentation
+"""
+
+
+
+def clean_data(data):
+"""
+"""
+
+def save_plot_to_file(dataframe, title, labels, start_date, end_date, filename):
+"""
+"""
+
+if __name__ == '__main__':
+
+import json
+json_data = json.load(filehandler)
+
+temps = [num]['labels']
+labels = s [num]['datasets']['label']
+valeurs = s [num]['datasets']['data']
+
+import pandas as pd
+humidity_dataframe = pd.DataFrame(
+data={
+label1: data1,
+label2: data2,
+label3: data3,
+},
+index=time_index,
+dtype='float'
+)
+humidity_dataframe.index = pd.to_datetime(humidity_dataframe.index)
+
+start_date = '2020-06-01'
+end_date = '2020-06-30'
+values = humidity_dataframe[start_date:end_date][label1].values
+
+start_date = '2020-06-01'
+end_date = '2020-06-30'
+values = humidity_dataframe[start_date:end_date].index #indices axe X
